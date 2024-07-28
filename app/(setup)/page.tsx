a91f431837cf38fe5343e33f7385f7d589133ef2
lyfxxxx <894624801@qq.com>
2024-07-28 16:29:37 +0800

    feat: 初始化

---
+++ app/(setup)/page.tsx
@@ -0,0 +1,25 @@
+import { db } from '@/lib/db';
+import { initalProfile } from '@/lib/inital-profile';
+import { redirect } from 'next/navigation';
+
+const SetupPage = async () => {
+  // 初始化用户
+  const profile = await initalProfile();
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (server) {
+    return redirect(`/servers/${server.id}`);
+  }
+
+  return <div>Create a Server</div>;
+};
+
+export default SetupPage;
