f9710afb863dc1468549172141b03e8dca27ed95
lyfxxxx <894624801@qq.com>
2024-08-18 18:37:15 +0800

    feat: 更新上传图片回显功能

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,4 +1,8 @@
 'use client';
+import { X } from 'lucide-react';
+
+import Image from 'next/image';
+
 import { UploadDropzone } from '@/lib/uploadthing';
 
 interface FileUploadProps {
@@ -8,6 +12,22 @@ interface FileUploadProps {
 }
 
 export const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
+  const fileType = value?.split('.').pop();
+
+  if (value && fileType !== 'pdf') {
+    return (
+      <div className="relative h-20 w-20">
+        <Image fill src={value} alt="upload" className="rounded-full" />
+        <button
+          onClick={() => onChange('')}
+          className="absolute right-0 top-0 rounded-full bg-rose-500 p-1 text-white shadow-sm"
+          type="button"
+        >
+          <X className="h-4 w-4" />
+        </button>
+      </div>
+    );
+  }
   return (
     <UploadDropzone
       endpoint={endpoint}
